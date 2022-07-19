9109c567970409cd77d706aca5bd9cfb72f3aea5
Alexseykka <59233823+alekseykka@users.noreply.github.com>
2022-07-19 18:47:52 +0300

    Создание парсера

--- parser_health_diet_ru/main.py
+++ parser_health_diet_ru/main.py
@@ -1,2 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
+
+# url = 'https://health-diet.ru/table_calorie/'
+# params = {
+#     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,'
+#               'image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+#                   '(KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+# }
+#
+# src = requests.get(url, params=params).text
+# with open('index.html', 'w', encoding='utf-8') as file:
+#     file.write(src)
+
